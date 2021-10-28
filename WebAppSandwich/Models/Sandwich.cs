22ec13a898c9c828fd832f2a4012aaaf7db06238
Ulf Bengtsson <Ulf.Bengtsson@Lexicon.se>
2021-10-28 10:35:41 +0200

    Add project files.

---
+++ WebAppSandwich/Models/Sandwich.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppSandwich.Models
+{
+    public class Sandwich
+    {
+        int id;
+        public int Id { get { return id; } }
+        public Ingrediens[] Ingrediens { get; set; }
+
+        public Sandwich(int id, params Ingrediens[] ingrediens)
+        {
+            this.id = id;
+            Ingrediens = ingrediens;
+        }
+    }
+}
---
+++ WebAppSandwich/wwwroot/js/WebPageCode.js
@@ -0,0 +1,33 @@
+﻿"use strict;"
+
+var sandwich = document.getElementById("sandwich");
+
+var listOfIngrediens = [
+    { id: 1, name: "Cheese" },
+    { id: 2, name: "Ham" },
+    { id: 3, name: "Mayonnaise" },
+    { id: 4, name: "Egg" }
+];
+
+var ingrediensToPutOnSandwich = [];
+
+function addIngrediensToSandwich(id) {
+    for (let ingrediens in listOfIngrediens) {
+        if (ingrediens.id === id) {
+            ingrediensToPutOnSandwich.push(ingrediens);
+        }
+    }
+    showCurrentSandwich();
+}
+
+function showCurrentSandwich() {
+
+    sandwich.innerHTML = "";//clear before
+
+    for (let ingrediens in ingrediensToPutOnSandwich) {
+        let layer = document.createElement("div");
+        layer.classList.add(ingrediens.name);
+        sandwich.appendChild(layer);
+    }
+
+}
